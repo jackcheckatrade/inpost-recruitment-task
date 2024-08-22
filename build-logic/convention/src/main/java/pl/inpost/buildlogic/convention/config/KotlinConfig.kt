f767d78dba1b82151d4dcef31eb3a61df44b7f1f
Jacek <jacek.lynx@gmail.com>
2024-08-22 23:58:27 +0200

    Add desugaring

--- build-logic/convention/src/main/java/pl/inpost/buildlogic/convention/config/KotlinConfig.kt
+++ build-logic/convention/src/main/java/pl/inpost/buildlogic/convention/config/KotlinConfig.kt
@@ -5,6 +5,7 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
@@ -23,12 +24,16 @@ internal fun Project.kotlinConfig(
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_17
             targetCompatibility = JavaVersion.VERSION_17
+            isCoreLibraryDesugaringEnabled = true
         }
 
         configure<KotlinAndroidProjectExtension> {
             compilerOptions {
                 jvmTarget.set(JvmTarget.JVM_17)
             }
+        }
+        dependencies {
+            add("coreLibraryDesugaring", libs.findLibrary("android-desugarJdkLibs").get())
         }
     }
 }
