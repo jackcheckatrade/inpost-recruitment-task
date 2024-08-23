46e8e23c82f66a6e6d68ee3af612bf8df140126f
Jacek <jacek.lynx@gmail.com>
2024-08-23 21:30:45 +0200

    Fix db bug

--- shipment/implementation/src/main/java/pl/inpost/shipment/implementation/data/MockedNetworkShipmentRepository.kt
+++ shipment/implementation/src/main/java/pl/inpost/shipment/implementation/data/MockedNetworkShipmentRepository.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.withContext
 import pl.inpost.shipment.api.ShipmentRepository
 import pl.inpost.shipment.api.model.Shipment
 import pl.inpost.shipment.implementation.data.local.ShipmentDao
-import pl.inpost.shipment.implementation.data.local.model.ShipmentLocal
+import pl.inpost.shipment.implementation.data.local.model.ShipmentWithSenderAndReceiver
 import pl.inpost.shipment.implementation.data.remote.api.ShipmentApi
 import javax.inject.Inject
 
@@ -17,32 +17,30 @@ class MockedNetworkShipmentRepository @Inject constructor(
 ) : ShipmentRepository {
 
     override suspend fun observeShipments(): Flow<List<Shipment>> {
-        return shipmentDao.observeShipments().map { shipmentLocalList ->
-            shipmentLocalList.map { shipmentLocal ->
-                shipmentLocal.toDomain(
-                    shipmentLocal.receiverEmail?.let { shipmentDao.getCustomer(it)?.toDomain() },
-                    shipmentLocal.senderEmail?.let { shipmentDao.getCustomer(it)?.toDomain() }
-                )
-            }
+        return shipmentDao.observeShipments().map { shipment ->
+            shipment.map { it.toDomain() }
         }
     }
 
     override suspend fun fetchShipments() =
         withContext(Dispatchers.IO) {
             val shipments = shipmentApi.getShipments().map {
-                ShipmentLocal(it.toDomain())
+                it.toDomain()
             }
-            val archivedShipments = shipmentDao.getShipments().filter { it.manualArchive }
-            shipmentDao.insertShipments(*shipments.map { shipmentLocal ->
-                if (shipmentLocal.number in archivedShipments.map { it.number }) {
-                    shipmentLocal.copy(manualArchive = true)
-                } else shipmentLocal
-            }.toTypedArray())
+            val archivedShipments = shipmentDao.getShipments().filter { it.shipment.manualArchive }
+            shipmentDao.insertShipments(
+                shipments.map { shipment ->
+                    if (shipment.number in archivedShipments.map { it.shipment.number }) {
+                        shipment.copy(operations = shipment.operations.copy(manualArchive = true))
+                    } else shipment
+                }.map { ShipmentWithSenderAndReceiver(it) }
+            )
         }
 
-    override suspend fun archiveShipment(shipmentNumber: String): Unit = withContext(Dispatchers.IO) {
-        shipmentDao.getShipment(shipmentNumber)?.let {
-            shipmentDao.insertShipments(it.copy(manualArchive = true))
+    override suspend fun archiveShipment(shipmentNumber: String): Unit =
+        withContext(Dispatchers.IO) {
+            shipmentDao.getShipment(shipmentNumber)?.let {
+                shipmentDao.insertShipments(it.copy(manualArchive = true))
+            }
         }
-    }
 }
--- shipment/implementation/src/main/java/pl/inpost/shipment/implementation/data/local/ShipmentDao.kt
+++ shipment/implementation/src/main/java/pl/inpost/shipment/implementation/data/local/ShipmentDao.kt
@@ -4,9 +4,12 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import kotlinx.coroutines.flow.Flow
+import pl.inpost.shipment.api.model.Shipment
 import pl.inpost.shipment.implementation.data.local.model.CustomerLocal
 import pl.inpost.shipment.implementation.data.local.model.ShipmentLocal
+import pl.inpost.shipment.implementation.data.local.model.ShipmentWithSenderAndReceiver
 
 @Dao
 interface ShipmentDao {
@@ -14,11 +17,24 @@ interface ShipmentDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertShipments(vararg shipments: ShipmentLocal)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCustomer(customer: CustomerLocal)
+
+    @Transaction
+    suspend fun insertShipments(shipments: List<ShipmentWithSenderAndReceiver>) {
+        shipments.forEach { shipment ->
+            insertShipments(shipment.shipment)
+            shipment.sender?.let { insertCustomer(it) }
+            shipment.receiver?.let { insertCustomer(it) }
+        }
+    }
+
+    @Transaction
     @Query("SELECT * FROM shipments")
-    fun observeShipments(): Flow<List<ShipmentLocal>>
+    fun getShipments(): List<ShipmentWithSenderAndReceiver>
 
     @Query("SELECT * FROM shipments")
-    fun getShipments(): List<ShipmentLocal>
+    fun observeShipments(): Flow<List<ShipmentWithSenderAndReceiver>>
 
     @Query("SELECT * FROM customers WHERE email = :email LIMIT 1")
     suspend fun getCustomer(email: String): CustomerLocal?
